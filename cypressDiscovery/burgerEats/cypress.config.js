773e075d80301e7a4f10f6e632a6ee245e6a6685
FlaviaFalotico <flaviafalotico@yahoo.com.br>
2022-08-18 19:56:58 -0300

    Cypress Project

--- cypressDiscovery/burgerEats/cypress.config.js
+++ cypressDiscovery/burgerEats/cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "fmqvfa",
-  viewportWidth: 1440,
-  viewportHeigth: 900,
 
   e2e: {
     setupNodeEvents(on, config) {
